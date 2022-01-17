a7a0ed69350993dac9666a11ec998c0e3b5f6203
Chloe Nott <chloe@nott.email>
2022-01-16 23:56:51 -0800

    Added packages and updated requirements.txt for whitenoise, simplejwt, and gunicorn. Implemented simplejwt authentication functionality. Verified refresh and access tokens function as intended with thunderclient.

--- notes_api_remade_and_extended/settings.py
+++ notes_api_remade_and_extended/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework_simplejwt',
     'notes',
 ]
 
@@ -137,5 +138,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
 }
